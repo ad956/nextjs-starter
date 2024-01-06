3826cb232bb636c4cf0b1b7302e0da8da49ec525
Anand Suthar <anandsuthar956@gmail.com>
2024-01-06 16:09:47 +0530

    users content added

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import getUsers from "@/lib/getUsers";
 import { User } from "@/types";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Users",
@@ -10,5 +11,28 @@ export const metadata: Metadata = {
 export default async function UsersPage() {
   const usersData: Promise<User[]> = getUsers();
   const users = await usersData;
-  return <div>UsersPage</div>;
+
+  // logs in the server terminal
+  console.log("Its a server component");
+
+  const content = (
+    <section>
+      <h2>
+        <Link href="/">HomePage</Link>
+      </h2>
+      <br />
+      {users.map((user) => {
+        return (
+          <>
+            <p key={user.id}>
+              <Link href={`/users/${user.id}`}>{user.name}</Link>
+            </p>
+            <br />
+          </>
+        );
+      })}
+    </section>
+  );
+
+  return content;
 }
