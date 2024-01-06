0eeb6fb72a3bdaf356bd5d7ced6e559f90c3682b
Anand Suthar <anandsuthar956@gmail.com>
2024-01-06 23:34:13 +0530

    added supense for userposts

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,9 +1,32 @@
+import getUser from "@/lib/getUser";
+import getUserPosts from "@/lib/getUserPosts";
+import { Post, User } from "@/types";
+import { Suspense } from "react";
+import UserPosts from "./components/UserPosts";
+
 type Params = {
   params: {
     userId: string;
   };
 };
 
-export default function UserPage({ params: { userId } }: Params) {
-  return <div>User</div>;
+export default async function UserPage({ params: { userId } }: Params) {
+  const userData: Promise<User> = getUser(userId);
+  const postsData: Promise<Post[]> = getUserPosts(userId);
+
+  // valid approach : parallel one
+  // const [user, posts] = await Promise.all([userData, postsData]);
+
+  // to use suspense, loading state , using below approach
+  const user = await userData;
+
+  return (
+    <>
+      <h2>{user.name}</h2>
+      <br />
+      <Suspense fallback={<h2>Loading Data ...</h2>}>
+        <UserPosts promise={postsData} />
+      </Suspense>
+    </>
+  );
 }
