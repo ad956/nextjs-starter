c32b10882c19df1ae8cdc8ea8390fe6e80f2860c
Anand Suthar <anandsuthar956@gmail.com>
2024-01-07 00:04:46 +0530

    added dynamic meta

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -3,12 +3,27 @@ import getUserPosts from "@/lib/getUserPosts";
 import { Post, User } from "@/types";
 import { Suspense } from "react";
 import UserPosts from "./components/UserPosts";
+import type { Metadata } from "next";
 
 type Params = {
   params: {
     userId: string;
   };
 };
+
+// export const metadata: Metadata = genrateMetaData();
+
+// dynamic metadata
+export async function genrateMetaData({
+  params: { userId },
+}: Params): Promise<Metadata> {
+  // OP feature of NextJS : the above request will be de-duped means the next will call the below line only omce even if its called multiple times
+  const userData: Promise<User> = getUser(userId);
+
+  const user = await userData;
+
+  return { title: user.name, description: `This is ${user.name} page` };
+}
 
 export default async function UserPage({ params: { userId } }: Params) {
   const userData: Promise<User> = getUser(userId);
