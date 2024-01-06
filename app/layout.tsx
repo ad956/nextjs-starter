4e2c04fa89c03bab046290f7c8f8ff582a69574a
Anand Suthar <anandsuthar956@gmail.com>
2024-01-06 13:17:42 +0530

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// earlier head.tsx was used, but its deprecated in NextJS 13.2
+// so use below
 export const metadata: Metadata = {
   title: "Next App",
   description: "NextJS app",
