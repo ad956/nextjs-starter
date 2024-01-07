30a995b0ccf7b31fedc0252dc46a913698686219
Anand Suthar <anandsuthar956@gmail.com>
2024-01-07 16:36:41 +0530

    generateStaticParams added for SSG

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -4,6 +4,8 @@ import { Post, User } from "@/types";
 import { Suspense } from "react";
 import UserPosts from "./components/UserPosts";
 import type { Metadata } from "next";
+import getUsers from "@/lib/getUsers";
+import { notFound } from "next/navigation";
 
 type Params = {
   params: {
@@ -20,7 +22,11 @@ export async function generateMetadata({
 }: Params): Promise<Metadata> {
   // OP feature of NextJS : the above request will be de-duped means the next will call the below line only omce even if its called multiple times
   const userData: Promise<User> = getUser(userId);
-  const user = await userData;
+  const user: User = await userData;
+
+  if (!user || !user.name) {
+    return { title: "User not found" };
+  }
 
   return { title: user.name, description: `This is ${user.name} page` };
 }
@@ -34,6 +40,9 @@ export default async function UserPage({ params: { userId } }: Params) {
 
   // to use suspense, loading state , using below approach
   const user = await userData;
+
+  if (!user || !user.name) return notFound();
+
   return (
     <>
       <h2>{user.name}</h2>
@@ -44,3 +53,36 @@ export default async function UserPage({ params: { userId } }: Params) {
     </>
   );
 }
+
+/*
+  The generateStaticParams function can be used in combination with dynamic route segments to statically generate routes at build 
+  time instead of on-demand at request time. // SSG 
+*/
+
+// when below isnt used its a SSR strategy but as its provided its a SSG strategy
+export async function generateStaticParams() {
+  // will be de-duped by nextjs
+  const usersData: Promise<User[]> = getUsers();
+  const users = await usersData;
+
+  return users.map((user) => ({
+    userId: user.id.toString(),
+  }));
+}
+
+/*
+
+dynamicParams :- Control what happens when a dynamic segment is visited that was not generated with generateStaticParams.
+
+                  ie. a user with id which doesn't exists
+
+used in :- layout.tsx | page.tsx
+
+usage :
+        export const dynamicParams = true // true | false,
+
+true (default): Dynamic segments not included in generateStaticParams are generated on demand.
+
+false: Dynamic segments not included in generateStaticParams will return a 404.
+
+*/
