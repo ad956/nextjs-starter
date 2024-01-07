b9dfc9bfdaf551554f80113e76b0215da9dd088a
Anand Suthar <anandsuthar956@gmail.com>
2024-01-07 21:24:10 +0530

    loading for searchpage

---
+++ app/compo/[searchTerm]/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <div className="text-2xl flex justify-center text-teal-600 animate-bounce">
+      Page Loading ....
+    </div>
+  );
+}
