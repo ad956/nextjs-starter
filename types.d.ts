f6ce3fd6426697ea207cc1f2d59ecaec10ce475f
Anand Suthar <anandsuthar956@gmail.com>
2024-01-07 20:18:04 +0530

    minor

--- types.d.ts
+++ types.d.ts
@@ -28,3 +28,34 @@ type Post = {
   title: string;
   body: string;
 };
+
+type Pexels = {
+  page: number;
+  per_page: number;
+  photos: [
+    {
+      id: number;
+      width: number;
+      height: number;
+      url: string;
+      photographer: string;
+      photographer_url: string;
+      photographer_id: number;
+      avg_color: string;
+      src: {
+        original: string;
+        large2x: string;
+        large: string;
+        medium: string;
+        small: string;
+        portrait: string;
+        landscape: string;
+        tiny: string;
+      };
+      liked: boolean;
+      alt: string;
+    }
+  ];
+  total_results: number;
+  next_page: string;
+};
